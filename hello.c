5375823023387d8d7fb0e96bf154f3f6db55f941
Trevor Martin <tmartin@hitex.co.uk>
2025-05-22 12:00:11 +0100

    recommit of speedup

--- hello.c
+++ hello.c
@@ -28,7 +28,7 @@ static void app_main_thread (void *argument) {
 
   for(int count = 0; count < 10; count++) {
     printf("Hello KeilChris %d\r\n", count);
-    osDelay(5000U);
+    osDelay(500U);
   }
   osDelay(osWaitForever);
 }
